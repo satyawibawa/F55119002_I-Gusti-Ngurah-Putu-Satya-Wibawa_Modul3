1de55312231ccaf772dbf762f513d198ee949713
satyawibawa <satyawibawa93@gmail.com>
2022-04-12 15:10:16 +0800

    Initial commit

---
+++ app/src/main/java/com/f55119002/fragmentlayoutexmodul3/FragmentExplore.java
@@ -0,0 +1,21 @@
+package com.f55119002.fragmentlayoutexmodul3;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentExplore extends Fragment {
+    View view;
+    public FragmentExplore() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.explore_fragment, container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/f55119002/fragmentlayoutexmodul3/FragmentHome.java
@@ -0,0 +1,21 @@
+package com.f55119002.fragmentlayoutexmodul3;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentHome extends Fragment {
+    View view;
+    public FragmentHome() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.home_fragment, container,false);
+        return view;
+    }
+}
